5df0be95769eed1f85e2ab164d5cebbb085fb489
MrKrip <45828333+MrKrip@users.noreply.github.com>
2021-10-14 13:40:40 +0300

    CI\CD?

--- ItransitionCourse/Startup.cs
+++ ItransitionCourse/Startup.cs
@@ -29,7 +29,7 @@ namespace ItransitionCourse
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("HostingConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.Configure<IdentityOptions>(options =>
             {
